82dbae0209c1ca7892c83c0bed81f7fba9c061a3
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-23 15:57:17 -0400

    added rest of information to other files, header, index, form, checkout.js / ran npm cypress open

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import Logo from "./img/Pizza.jpg";
-import Form from "./Form";
+import Form from "./Form.js";
 import "./App.css";
 
 function SliceHeader() {
@@ -15,7 +15,7 @@ function SliceHeader() {
           </div>
 
           <div className="link2">
-            <Link to="/Form">Order Now Online</Link>
+            <Link to="./Form.js">Order Now Online</Link>
             <img src={Logo} alt="pizza" />
             <Route path="./Form.js">
               <Form />
