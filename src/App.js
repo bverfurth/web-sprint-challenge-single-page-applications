44005c7e6d37047a3a0c776badfdbc8958586f3d
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-23 14:17:34 -0400

    Setting up app.js and adding content to header.js - will have to add more files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="header">
+      <SliceHeader />
+    </div>
   );
 };
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function SliceHeader() {
+  return (
+    <Router>
+      <div className="nav-links">
+        <div className="navBar">
+          <div className="link1">
+            <h1>The Last Slice</h1>
+            <Link to="/">Home</Link>
+          </div>
+
+          <div className="link2">
+            <Link to="/Form">Order Now Online</Link>
+            <img src={Logo} alt="pizza" />
+            <Route path="/Form">
+              <PizzaForm />
+            </Route>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+export default SliceHeader;
